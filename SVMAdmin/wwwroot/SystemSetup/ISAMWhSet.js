f42c4065325710e1055b8ca570774f2944d45acd
BenLin <ben.lin@mail.e-dynasty.com.tw>
2022-07-26 16:15:17 +0800

    2022/07/26

--- SVMAdmin/wwwroot/SystemSetup/ISAMWhSet.js
+++ SVMAdmin/wwwroot/SystemSetup/ISAMWhSet.js
@@ -4,8 +4,8 @@ var PageISAMWHSET = function (ParentNode) {
     let AssignVar = function () {
         alert("AssignVar");
         //$('#selST_ID').change(function () {
-        //    let st_id = $('#selST_ID').val();
         //    alert("st_id");
+        //    let st_id = $('#selST_ID').val();
         //    //InitSelectItem($('#selLayerNo')[0], dtMachineListSpecBlank, "LayerNo", "LayerNo", true, "*請選擇貨倉代號");
         //    if (st_id == "") {
         //    }
@@ -13,57 +13,67 @@ var PageISAMWHSET = function (ParentNode) {
         //        var pData = {
         //            ST_ID: st_id
         //        };
-        //        alert("PostToWebApi");
-        //        PostToWebApi({ url: "api/SystemSetup/GetWh", data: pData, success: AfterGetCkNoByST_ID });
+        //        //alert("PostToWebApi");
+        //        //PostToWebApi({ url: "api/SystemSetup/GetWh", data: pData, success: AfterGetCkNoByST_ID });
         //    }
         //});
 
-         alert("AfterGetCkNoByST_ID");
        //let AfterGetCkNoByST_ID = function (data) {
-       //     let dtWarehouseDSV = data.getElementsByTagName('dtWh');
+       //   alert("AfterGetCkNoByST_ID");
+       //    let dtWarehouseDSV = data.getElementsByTagName('dtWh');
        // }
 
-        alert("btSaveISAMWH");
-    //  $('#btSaveISAMWH').click(function () {
-    //        let records = [];
-    //        let cbs = $('#ContainerChanSet .CubeChannel-1');
-    //        for (let i = 0; i < cbs.length; i++) {
-    //            var cb = cbs[i];
-    //            if ($(cb).hasClass('CubeChannel-1_mask')) {
-    //                continue;
-    //            }
-    //            let rec = $(cb).prop('Record');
-    //            let MachineInfo = $(cb).prop('MachineInfo');
-    //            if (rec != null) {
-    //                //let record = {
-    //                //    WhNO: ST_ID,
-    //                //    PLU: GetNodeValue(rec, "GD_NO"),
-    //                //    PTNum: 0,
-    //                //    SafeNum: 1,
-    //                //    In_Date: "",
-    //                //    Out_Date: "",
-    //                //    StartSalesDate: "",
-    //                //    EndSalesDate: "",
-    //                //    DisPlayNum: $(cb).find('.ChanQty').val(),
-    //                //    CkNo: CkNo,
-    //                //    Layer: LayerNo,
-    //                //    Sno: GetNodeValue(MachineInfo, "ChannelNo"),
-    //                //    EffectiveDate: ""
-    //                //}
-    //                //records.push(record);
-    //            }
-    //        }
-    //        var pData = {
-    //            InventorySV: records
-    //        };
-    //        PostToWebApi({ url: "api/SystemSetup/SaveNewInventorySV", data: pData, success: AfterSaveNewInventorySV });
-    //    });
+      $('#btSaveISAMWH').click(function () {
+         alert("btSaveISAMWH");
+           //let records = [];
+            //let cbs = $('#ContainerChanSet .CubeChannel-1');
+            //for (let i = 0; i < cbs.length; i++) {
+                //var cb = cbs[i];
+                //if ($(cb).hasClass('CubeChannel-1_mask')) {
+                //    continue;
+                //}
+                //let rec = $(cb).prop('Record');
+                //let MachineInfo = $(cb).prop('MachineInfo');
+                //if (rec != null) {
+                //    let record = {
+                //        ST_ID: ST_ID
+                //        //PLU: GetNodeValue(rec, "GD_NO"),
+                //        //PTNum: 0,
+                //        //SafeNum: 1,
+                //        //In_Date: "",
+                //        //Out_Date: "",
+                //        //StartSalesDate: "",
+                //        //EndSalesDate: "",
+                //        //DisPlayNum: $(cb).find('.ChanQty').val(),
+                //        //CkNo: CkNo,
+                //        //Layer: LayerNo,
+                //        //Sno: GetNodeValue(MachineInfo, "ChannelNo"),
+                //        //EffectiveDate: ""
+                //    }
+                //    records.push(record);
+                //}
+            //}
+            let rec = $('#selST_ID').val();
+            var pData = {
+                InventorySV: rec
+            };
+            PostToWebApi({ url: "api/SystemSetup/SaveNewInventorySV", data: pData, success: AfterSaveNewInventorySV });
+        });
     };
 
+    let AfterSaveNewInventorySV = function (data) {
+        if (ReturnMsg(data, 0) != "SaveNewInventorySVOK") {
+            DyAlert(ReturnMsg(data, 0));
+        }
+        else {
+            DyAlert("完成!");
+        }
+    }
+
     let afterGetInitWhSet = function (data) {
-        alert("afterGetInitWhSet");
+        //alert("afterGetInitWhSet");
         dtWh = data.getElementsByTagName('dtWh');
-        alert("InitSelectItem");
+        //alert("InitSelectItem");
         InitSelectItem($('#selST_ID')[0], dtWh, "ST_ID", "STName", true, "*請選擇店代號");
         AssignVar();
         //$('#btSaveISAMW').click(function () { btSave_click(); });
