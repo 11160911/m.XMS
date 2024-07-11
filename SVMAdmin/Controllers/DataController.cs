0c594c9368a15dc9d608022b5ffa45d4828a4975
s90050357 <86585850+s90050357@users.noreply.github.com>
2024-07-11 16:23:04 +0800

    MSSA101 2,3層

--- SVMAdmin/Controllers/DataController.cs
+++ SVMAdmin/Controllers/DataController.cs
@@ -13328,12 +13328,12 @@ namespace SVMAdmin.Controllers
                 sql += "case when isnull(sum(Recs),0)=0 then 0 else round(isnull(sum(Cash),0)/isnull(sum(Recs),0),0) end  CusCash1,";
                 sql += "isnull(sum(VIP_Cash),0) VCash,isnull(sum(VIP_Recs),0) VCnt,";
                 sql += "case when isnull(sum(VIP_Recs),0)=0 then 0 else round(isnull(sum(VIP_Cash),0)/isnull(sum(VIP_Recs),0),0) end VCusCash,";
-                sql += "case when isnull(sum(Cash),0)=0 then format(0,'p2') else format(isnull(sum(VIP_Cash),0)/isnull(sum(Cash),0),'p2') end VPer into #tmpSel ";
+                sql += "case when isnull(sum(Cash),0)=0 then format(0,'p2') else format(cast(isnull(sum(VIP_Cash),0) as float)/cast(isnull(sum(Cash),0) as float),'p2') end VPer into #tmpSel ";
                 sql += "from (" + sqlBaseData + ") a "+sqlGroup+";";
                 sql += "insert into #tmpSel select 'SumAll',isnull(sum([Cash1]),0), isnull(sum([Cnt1]),0),";
                 sql += "case when isnull(sum([Cnt1]),0)= 0 then 0 else round(isnull(sum(Cash1), 0) / isnull(sum([Cnt1]), 0),0) end,";
                 sql += "isnull(sum([VCash]),0), isnull(sum([VCnt]),0),case when isnull(sum([VCnt]),0)= 0 then 0 else round(isnull(sum(VCash), 0) / isnull(sum([VCnt]), 0),0) end,";
-                sql += "case when isnull(sum(Cash1),0)= 0 then format(0,'p2') else format(isnull(sum(VCash),0)/isnull(sum(Cash1),0),'p2') end from #tmpSel;";
+                sql += "case when isnull(sum(Cash1),0)= 0 then format(0,'p2') else format(cast(isnull(sum(VCash),0) as float)/cast(isnull(sum(Cash1),0) as float),'p2') end from #tmpSel;";
                 sql += "select * from #tmpSel order by [ID];";
                 DataTable dtDelt = PubUtility.SqlQry(sql, uu, "SYS");
                 dtDelt.TableName = "dtDelt";
